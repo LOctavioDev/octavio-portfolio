50ae1b88120d68931f9236f05d91363a5e8f824a
Luis Octavio <octaviodevtech@gmail.com>
2024-09-06 01:34:13 -0600

    ADD: Primera maquetacion

---
+++ components/header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import MotionTransition from "./transition-component";
+
+const Header = () => {
+    return ( 
+        <MotionTransition position="bottom" className="absolute z-40 inline-block w-full top-5 md:top-10">
+            <header>
+                <div className="container justify-between max-w-6xl mx-auto md:flex">
+                    <Link href="/">Octavio Dev</Link>
+                </div>
+            </header>
+        </MotionTransition>
+     );
+}
+ 
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: '#4c95d1',
+        darkBg: '#131425',
+      },
+      backgroundImage: {
+        'gradient-cover':
+          'linear-gradient(90.21deg, rgba(40, 54, 124, 0.5) 0%, rgba(53, 27, 89, 0.5) 100%)',
       },
     },
   },
